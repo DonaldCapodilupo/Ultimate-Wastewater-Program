df5a310180185b929984deead44ebbe6cfe12fab
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2022-11-19 17:08:20 -0500

    Lab Data cleaned up, Press data ready for Flask integration.

--- static/js/Billerica-Division-Lab-Sheet.js
+++ static/js/Billerica-Division-Lab-Sheet.js
@@ -8,6 +8,37 @@ function create_Table(div_to_fill_id, title, subtitle, list_of_labels) {
     div_to_fill.innerHTML +=
         "<!--" + title + "-->" +
         '<div class="row">' +
+        '    <div class="p-3 mb-2 bg-success text-white d-flex justify-content-center">' +
+        '        <h6>' + title + '</h6>' +
+        '    </div>' +
+        '</div>';
+
+    //Table Vertical Labels.
+    for (let i = 0; i < list_of_labels.length; i++) {
+        div_to_fill.innerHTML +=
+            "<!--" + list_of_labels[i] + "-->" +
+            '<div class="row mt-1" class="input_data_row">' +
+            '   <div class="col" id="data_labels">' +
+            '       <label for="'+ title + ' ' + list_of_labels[i] + '" class="text_label" class="display_label">' + list_of_labels[i] + ':</label>' +
+            '   </div>' +
+
+            '   <div class="col" id="data_user_input" >' +
+            '       <input type="text" class="data_input_field" id="' + title + ' ' + list_of_labels[i] +'">' +
+            '   </div>'+
+            '</div>';
+
+
+    }
+}
+
+
+function create_Table_2(div_to_fill_id, title, subtitle, list_of_labels) {
+    const div_to_fill = document.getElementById(div_to_fill_id);
+
+    //Title Of The Table
+    div_to_fill.innerHTML +=
+        "<!--" + title + "-->" +
+        '<div class="row">' +
         '    <div class="p-3 mb-2 bg-info text-white d-flex justify-content-center">' +
         '        <h4>' + title + '</h4>' +
         '    </div>' +
@@ -41,12 +72,11 @@ function create_Table(div_to_fill_id, title, subtitle, list_of_labels) {
             '        </div>' +
             '    </div>' +
             '    <div class="col">' +
-            '        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">'+
+            '        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">' +
             '    </div>' +
             '</div>';
     }
 }
-
 
 
 //draggable divs
@@ -54,44 +84,44 @@ function create_Table(div_to_fill_id, title, subtitle, list_of_labels) {
 
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
 
 

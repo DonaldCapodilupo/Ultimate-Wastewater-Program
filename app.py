fc91924485fe5dab6470b533cdff8cf8afd473e8
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2022-12-08 20:55:11 -0500

    Changed the json files to save as the date. Added a /Data route to start visualizing the data.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import datetime
 
 app = Flask(__name__)
 
@@ -7,7 +8,6 @@ app = Flask(__name__)
 def main_Menu():
     if request.method == "POST":
         if request.form['submit_button'] == "Save Data":
-
             import json
 
             data = {}
@@ -16,14 +16,28 @@ def main_Menu():
             for key,value in data_to_store.items():
                 data[key] = value
 
-            print(data)
-            with open('data.json', 'w') as f:
+            today_date = str(datetime.date.today())
+            with open("Historical Data/" + today_date + '.json', 'w') as f:
                 json.dump(data, f)
+
             return render_template("main.html")
-        # return redirect(url_for("newspaper_Website_Scraper"))
+
 
     else:
         return render_template("main.html")
+
+
+
+@app.route('/Data', methods=["POST","GET"])
+def data_Menu():
+    if request.method == "POST":
+        pass
+
+    else:
+        from Backend import get_Historical_Data
+        historical_data = get_Historical_Data()
+        print(historical_data["Influent Time"])
+        return render_template("test.html", data=historical_data ) #list_of_dates=['11/6','11/7','11/12','11/13'], list_of_data =historical_data["Influent Time"]
 
 
 if __name__ == '__main__':

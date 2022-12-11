42f2bdb064b2452666c952b8fa2ec181b45d0cdf
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2022-12-11 12:37:56 -0500

    Added a main menu. Lab sheet is now its seperate function. The main menu can activate both current routes and those pages can return to the main menu.

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import datetime
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=["POST","GET"])
-def main_Menu():
+@app.route('/Daily-Lab-Sheet', methods=["POST","GET"])
+def daily_Lab_Sheet():
     if request.method == "POST":
         if request.form['submit_button'] == "Save Data":
             import json
@@ -20,24 +19,47 @@ def main_Menu():
             with open("Historical Data/" + today_date + '.json', 'w') as f:
                 json.dump(data, f)
 
-            return render_template("main.html")
+            return render_template("Daily Lab Sheet.html")
+
+        if request.form['submit_button'] == "Return Home":
+
+
+            return redirect(url_for('main_Menu'))
 
 
     else:
-        return render_template("main.html")
+        return render_template("Daily Lab Sheet.html")
+
+@app.route('/', methods=["POST","GET"])
+def main_Menu():
+    if request.method == "POST":
+        if request.form['submit_button'] == "Main Lab Sheet":
+
+
+            return redirect(url_for('daily_Lab_Sheet'))
+
+        elif request.form['submit_button'] == "View Data":
+
+
+            return redirect(url_for('data_Menu'))
+
+
+    else:
+        return render_template("Main Menu.html")
 
 
 
 @app.route('/Data', methods=["POST","GET"])
 def data_Menu():
     if request.method == "POST":
-        pass
+        if request.form['submit_button'] == "Return Home":
+            return redirect(url_for('main_Menu'))
 
     else:
         from Backend import get_Historical_Data
         historical_data = get_Historical_Data()
         print(historical_data["Influent Time"])
-        return render_template("test.html", data=historical_data ) #list_of_dates=['11/6','11/7','11/12','11/13'], list_of_data =historical_data["Influent Time"]
+        return render_template("Data Viewer.html", data=historical_data) #list_of_dates=['11/6','11/7','11/12','11/13'], list_of_data =historical_data["Influent Time"]
 
 
 if __name__ == '__main__':

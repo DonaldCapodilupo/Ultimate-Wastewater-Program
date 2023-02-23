fb33ee1b33c6d04f629548cf65a59ce88b2affad
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2023-02-23 09:39:53 -0500

    Added buttons and HTML files for all functions that are expected to be a part of the program at this time.

--- app.py
+++ app.py
@@ -7,15 +7,26 @@ app = Flask(__name__)
 @app.route('/', methods=["POST","GET"])
 def main_Menu():
     if request.method == "POST":
-        if request.form['submit_button'] == "Main Lab Sheet":
 
+        button_clicked = request.form['submit_button']
 
+        if button_clicked == "Main Lab Sheet":
             return redirect(url_for('daily_Lab_Sheet'))
 
-        elif request.form['submit_button'] == "View Data":
-
-
+        elif button_clicked == "View Data":
             return redirect(url_for('data_Menu'))
+
+        elif button_clicked == "Access Time Clock":
+            return redirect(url_for('time_Clock'))
+
+        elif button_clicked == "Take Practice Test":
+            return redirect(url_for('practice_Quiz'))
+
+        elif button_clicked == "Check Permit Compliance":
+            return redirect(url_for('check_Compliance'))
+
+        elif button_clicked == "Check Weekly Tasks":
+            return redirect(url_for('check_Checklists'))
 
 
     else:
@@ -49,8 +60,6 @@ def daily_Lab_Sheet():
     else:
         return render_template("Daily Lab Sheet.html")
 
-
-
 @app.route('/Data', methods=["POST","GET"])
 def data_Menu():
     if request.method == "POST":
@@ -62,6 +71,35 @@ def data_Menu():
         historical_data = get_Historical_Data()
         print(historical_data["Influent Time"])
         return render_template("Data Viewer.html", data=historical_data) #list_of_dates=['11/6','11/7','11/12','11/13'], list_of_data =historical_data["Influent Time"]
+
+@app.route('/Time-Clock', methods=["POST","GET"])
+def time_Clock():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("Time Clock.html")
+
+@app.route('/Practice-Quiz', methods=["POST","GET"])
+def practice_Quiz():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("Practice Quiz.html")
+
+@app.route('/Compliance', methods=["POST","GET"])
+def check_Compliance():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("Compliance.html")
+
+
+@app.route('/Checklists', methods=["POST","GET"])
+def check_Checklists():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("Checklists.html")
 
 
 if __name__ == '__main__':
